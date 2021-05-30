75c6671a3ce8d2a2d142b5176a63fbf0ae122db0
Ehsan Edalat <2539535+ehsanedalat@users.noreply.github.com>
2021-05-30 15:44:47 +0430

    Update fuzz_with_radamsa.sh

--- Fuzzing/radamsa+sanitizer/fuzz_with_radamsa.sh
+++ Fuzzing/radamsa+sanitizer/fuzz_with_radamsa.sh
@@ -1,9 +1,19 @@
 while true
 do
-	q= echo "100" | "radamsa"
-	w= echo "100" | "radamsa"
-	./a.out $q $w
-	X=$?
-	echo "RES: "$X
+	radamsa -o tc-%n -n 1000 ./testCases/sample-*
+	q=1
+	w=2
+	while true
+	do
+		E=$(head -n 1 tc-$q)
+		R=$(head -n 1 tc-$w)
+		./stack_overflow_with_sanitizer.out $E $R
+		X=$?
+		q=$((q+1))
+		w=$((w+1))
+		echo "RES: "$X
+		test $X -gt 0 && break
+		test $q -lt 1000 && break
+	done
 	test $X -gt 0 && break
 done
